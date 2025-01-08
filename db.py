1e59cace59d74cb59778a2144d6a558f564f37fc
Rishabh Singh <rishabh@Rishabhs-MacBook-Pro.local>
2025-01-07 20:03:55 -0500

    feat: Add receipt tagging system with auto-rules for loyalty, spending & weekend shopping + tests

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, Table, ForeignKey, Index
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
 from datetime import datetime
 import os
 
